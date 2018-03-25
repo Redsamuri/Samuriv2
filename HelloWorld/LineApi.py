45b5589dd0043a038f8bf384ae65f037d0ba245e
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-03-25 19:25:47 +0700

    Update LineApi.py

--- HelloWorld/LineApi.py
+++ HelloWorld/LineApi.py
@@ -412,7 +412,7 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("   [   Hello World   ]   ")
+      print("   [  Redsamuri  ]   ")
       print("          Thanks for my friend")
       print("=========================================")
       print("MID : " + prof.mid)
